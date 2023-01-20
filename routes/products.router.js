867f09c460c2f4f82fc07687cf4786c7b13c0d9c
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-01-21 01:50:39 +0600

    new cdoe aded

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
 const router = express.Router();
 const dbConnect = require("../utils/dbConnect");
 const client = dbConnect();
@@ -24,13 +25,25 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/:email", async (req, res) => {
+router.get("/get-specific-user-products", async (req, res) => {
   try {
-    const email = req.params.email;
+    const email = req.query.email;
     const filter = { email: email };
+
     const product = await productCollection.find(filter).toArray();
-    const result = product.reverse();
-    res.status(200).send(result);
+    res.status(200).send(product);
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
+
+router.get("/delete-specific-user-products", async (req, res) => {
+  try {
+    const id = req.query.id;
+    const filter = { _id: ObjectId(id) };
+
+    const result = await productCollection.deleteOne(filter);
+    res.status(200).send("Delete Successfull !");
   } catch (error) {
     res.status(404).send(error);
   }
