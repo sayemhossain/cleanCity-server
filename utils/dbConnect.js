85b8fb2eaf97a9c2bd98c8f7ce772446ec49d219
SAYAM HOSSAIN <shayem15-12011@diu.edu.bd>
2023-01-20 22:45:22 +0600

    new code added

--- utils/dbConnect.js
+++ utils/dbConnect.js
@@ -1,5 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
+//For connectting Database
 const dbConnect = () => {
   const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.v36mb1p.mongodb.net/?retryWrites=true&w=majority`;
 
